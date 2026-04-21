e198cd32b518600fa422d24370315d99547d5d27
matt-palmer999 <matt_palmer@outlook.com>
2026-04-21 13:06:31 +0200

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1385,6 +1385,9 @@ def build_html(films_by_title: dict, anchor: datetime) -> str:
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <link rel="icon" type="image/png" href="/favicon.png">
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="manifest" href="/manifest.json">
